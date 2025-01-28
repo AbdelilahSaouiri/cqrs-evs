ed3f326666e2b24e97f59c382a9f9ca19e8658ee
med <abdosaouiri757@gmail.com>
2025-01-28 20:31:57 +0100

    add sec to shipment-query-service

--- shipement-service/shipement-query-service/src/main/java/net/ensah/shipementqueryservice/controller/ShipmentQueryController.java
+++ shipement-service/shipement-query-service/src/main/java/net/ensah/shipementqueryservice/controller/ShipmentQueryController.java
@@ -7,6 +7,7 @@ import net.ensah.shipementqueryservice.entity.Shipment;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class ShipmentQueryController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public CompletableFuture<List<Shipment>> getAllShipments(
             @RequestParam(name = "page",defaultValue = "0") Integer page,
             @RequestParam(value = "size",defaultValue = "2") Integer size) {
@@ -34,12 +36,14 @@ public class ShipmentQueryController {
     }
 
     @GetMapping("/id/{id}")
+    @PreAuthorize("hasAnyAuthority('USER')")
     public CompletableFuture<ResponseEntity<Shipment>> getShipmentById(@PathVariable("id") String id){
         CompletableFuture<Shipment> query = queryGateway.query(new GetShipmentById(id), ResponseTypes.instanceOf(Shipment.class));
         return query.thenApply(ResponseEntity::ok);
     }
 
     @GetMapping("/phone/{phone}")
+    @PreAuthorize("hasAnyAuthority('USER')")
     public CompletableFuture<ResponseEntity<Shipment>> getShipmentByRecipientPhoneNumber(@PathVariable("phone") String phone){
         CompletableFuture<Shipment> query = queryGateway.query(new GetShipmentByRecipientPhoneNumber(phone), ResponseTypes.instanceOf(Shipment.class));
         return query.thenApply(ResponseEntity::ok);
--- shipement-service/shipement-query-service/src/main/java/net/ensah/shipementqueryservice/security/SecurityConfig.java
+++ shipement-service/shipement-query-service/src/main/java/net/ensah/shipementqueryservice/security/SecurityConfig.java
@@ -8,19 +8,52 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    private final JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests()
+                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/h2-console/**","/swagger-ui/**","/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2->oauth2.jwt(
+                        jwt->jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                ))
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","DELETE"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
